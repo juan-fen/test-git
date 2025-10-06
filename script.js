ece1d7cac2588821c79221c5a2b653eed76db768
Juan <juan@learnfrontendnow.com>
2025-10-06 18:36:02 -0500

    Add script tag

---
+++ script.js
@@ -0,0 +1,16 @@
+// const HTMLButton = document.querySelector(".button");
+// const wrapper = document.querySelector(".wrapper");
+
+// HTMLButton.addEventListener("click", () => {
+//   const deleteDiv = document.createElement("div");
+//   const newButton = document.createElement("button");
+//   deleteDiv.textContent = `${Math.floor(Math.random() * 10)}`;
+//   newButton.textContent = "Delete all";
+//   wrapper.appendChild(deleteDiv);
+//   wrapper.appendChild(newButton);
+
+//   newButton.addEventListener("click", () => {
+//     deleteDiv.remove();
+//     newButton.remove();
+//   });
+// });
